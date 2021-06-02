6c9626bc2214df7abff3a5c466f23fac0792ed27
Benga <benga.olasebikan@gmail.com>
2021-06-02 21:32:21 +0100

    Register page setup and ready to start backend server

--- client/pages/register.js
+++ client/pages/register.js
@@ -6,15 +6,32 @@ const Register = () => {
   const [formText, setFormText] = useState({
     name: '',
     email: '',
-    password: ''
+    password: '',
+    error: ''
   })
+
+  const { name, email, password } = formText
 
   const handleChange = e => {
     setFormText({ ...formText, [e.target.name]: e.target.value })
   }
 
+  console.log(Boolean(formText.error))
 
-  const { name, email, password } = formText
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log('Form: Submitted', formText)
+    if (name.length < 4) {
+      setFormText({ ...formText, error: 'Please add name with at least 5 characters' })
+    }
+    setFormText({
+      name: '',
+      email: '',
+      password: '',
+      error: ''
+    })
+  }
+
 
 
   return (
@@ -22,7 +39,8 @@ const Register = () => {
       <Layout>
         <Col className="m-auto" md="6">
           <h1 className="mb-4">Register</h1>
-          <Form className="w-75 reg-form p-5">
+          {formText.error && <p>{formText.error}</p>}
+          <Form onSubmit={handleSubmit} className="w-75 reg-form p-5">
             <FormGroup>
               <Label>Name</Label>
               <Input onChange={handleChange} value={name} name="name" type="text" placeholder="Type your name" />
